008befcd803255f570e13c4384b83e7eecc981f3
EdmundGoodman <egoodman3141@gmail.com>
2021-07-16 11:03:08 +0100

    Continued work on navbar, including adding scroll indicator, and worked on footer

--- static/js/main.js
+++ static/js/main.js
@@ -1,12 +1,17 @@
-  // Add slideDown animation to Bootstrap dropdown when expanding.
-  $('.dropdown').on('show.bs.dropdown', function() {
+// Add slideDown animation to Bootstrap dropdown when expanding.
+$('.dropdown').on('show.bs.dropdown', function() {
     $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
-  });
+});
 
-  // Add slideUp animation to Bootstrap dropdown when collapsing.
-  $('.dropdown').on('hide.bs.dropdown', function() {
+// Add slideUp animation to Bootstrap dropdown when collapsing.
+$('.dropdown').on('hide.bs.dropdown', function() {
     $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
-  });
+});
 
-
-  $.stellar();
+// Capture scroll any percentage
+$(window).scroll(function(){
+    var wintop = $(window).scrollTop(), docheight =
+        $(document).height(), winheight = $(window).height();
+        var scrolled = (wintop/(docheight-winheight))*100;
+        $('#scroll-line').css('width', (scrolled + '%'));
+});

4468af5a16df426f0e008043162bd82dd3dde303
EdmundGoodman <egoodman3141@gmail.com>
2021-07-22 07:54:08 +0100

    Continued work on adding more sections for index page etc.

--- static/js/main.js
+++ static/js/main.js
@@ -1,17 +1,17 @@
-// Add slideDown animation to Bootstrap dropdown when expanding.
+
 $('.dropdown').on('show.bs.dropdown', function() {
     $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
 });
-
-// Add slideUp animation to Bootstrap dropdown when collapsing.
 $('.dropdown').on('hide.bs.dropdown', function() {
     $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
 });
 
+
 // Capture scroll any percentage
+const $scrollLine = $('#scroll-line');
 $(window).scroll(function(){
     var wintop = $(window).scrollTop(), docheight =
         $(document).height(), winheight = $(window).height();
         var scrolled = (wintop/(docheight-winheight))*100;
-        $('#scroll-line').css('width', (scrolled + '%'));
+        $scrollLine.css('width', (scrolled + '%'));
 });

9b422a3a74755d23ccc7b4817704d1247ce1fa0a
EdmundGoodman <egoodman3141@gmail.com>
2021-10-07 20:57:02 +0100

    Added animations to the front page

--- static/js/main.js
+++ static/js/main.js
@@ -3,8 +3,10 @@ $(document).ready(function () { // Require all js to load
     // Require all images to load
     // (this should be the big one for most page loading tbh)
     Promise.all(Array.from(document.images).filter(img => !img.complete).map(img => new Promise(resolve => { img.onload = img.onerror = resolve; }))).then(() => {
-        setTimeout(function(){
+        setTimeout(function () {
+            //
             $('body').addClass('loaded');
+            $('#homepage-hero-text').addClass('animate__animated animate__fadeInLeft');
         }, 1500); //TODO: Work out what works for this delay on the actual site
     });
 });
@@ -23,7 +25,10 @@ $('.read_more').find('a[href="#"]').on('click', function (e) {
 });
 
 
+
+
 $(function () {
+    // Handle the sticky header
     var siteSticky = function () {
         $(".js-sticky-header").sticky({ topSpacing: 0 });
     };
@@ -95,5 +100,40 @@ $(function () {
         });
     };
     siteMenuClone();
+
+
+
+
+    /*
+
+    // Handle animations scrolling into view
+    var elements;
+    var windowHeight;
+    var offset;
+
+    function init() {
+        elements = document.querySelectorAll('.waiting-animation');
+        windowHeight = window.innerHeight;
+        offset = windowHeight * (40/100);
+    }
+
+    function checkPosition() {
+        for (var i = 0; i < elements.length; i++) {
+            var element = elements[i];
+            var positionFromTop = elements[i].getBoundingClientRect().top;
+
+            if (positionFromTop - windowHeight + offset <= 0) {
+                element.classList.remove('waiting-animation');
+            }
+        }
+    }
+
+    window.addEventListener('scroll', checkPosition);
+    window.addEventListener('resize', init);
+
+    init();
+    checkPosition();
+
+    */
 
 });
